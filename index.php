016ebd3a14f1fabdc97e7d052cce392a459f7270
alammm24 <alammm24@gmail.com>
2019-12-30 02:45:22 +0100

    thirdone

--- index.php
+++ index.php
@@ -6,68 +6,137 @@
 
 <body>
 
-<div id="nowPlayingBarContainer">
+
+<div id="mainContainer">
+
+<div id="topContainer">
+
+	<div id="navBarContainer">
+		<nav class="navBar">
+
+			<a href="index.php" class="logo">
+				<img src="assets/images/icons/logo.png">
+			</a>
+
+
+			<div class="group">
+
+				<div class="navItem">
+					<a href="search.php" class="navItemLink">Search
+						<img src="assets/images/icons/search.png" class="icon" alt="Search">
+					</a>
+				</div>
+
+			</div>
+
+			<div class="group">
+				<div class="navItem">
+					<a href="browse.php" class="navItemLink">Browse</a>
+				</div>
+
+				<div class="navItem">
+					<a href="yourMusic.php" class="navItemLink">Your Music</a>
+				</div>
+
+				<div class="navItem">
+					<a href="profile.php" class="navItemLink">MOHAMED AALLAM</a>
+				</div>
+			</div>
+
+
+
+
+		</nav>
+	</div>
+
+</div>
+
+		<div id="nowPlayingBarContainer">
 
 		<div id="nowPlayingBar">
 
-<div id="nowPlayingLeft">
-   <div class="content">
-		<span class="albumLink" >
-			<img src="https://assets.audiomack.com/mullastar/rudeboy-psquare-realitymullastar-275-275-1531856756.jpg" class="albumArtwork">
-		</span>
-        <div class="trackInfo">
-
-			<span class="trackName">
-				<span>Who am i</span>
+		<div id="nowPlayingLeft">
+		<div class="content">
+			<span class="albumLink" >
+				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwdGdH-OiivPe7Anc4rylxRRgQ4zTtE3ywaC35LjqdhyO-mJ5s&s" class="albumArtwork">
 			</span>
+			<div class="trackInfo">
 
-			<span class="artistName">
-				<span>Mohamed aallam</span>
-			</span>
+				<span class="trackName">
+					<span>Who am i</span>
+				</span>
+
+				<span class="artistName">
+					<span>Mohamed aallam</span>
+				</span>
+
+			</div>
+
+
 
 		</div>
+		</div>
+
+				<div id="nowPlayingCenter">
+
+					<div class="content playerControls">
+
+						<div class="buttons">
+
+							<button class="controlButton shuffle" title="lecture aléatoire">
+								<img src="assets/images/icons/shuffle.png" alt="Shuffle">
+							</button>
+
+							<button class="controlButton previous" title="Précédent">
+								<img src="assets/images/icons/previous.png" alt="Previous">
+							</button>
+
+							<button class="controlButton play" title="Lecture">
+								<img src="assets/images/icons/play.png" alt="Play">
+							</button>
+
+							<button class="controlButton pause" title="Pause " style="display: none;">
+								<img src="assets/images/icons/pause.png" alt="Pause">
+							</button>
+
+							<button class="controlButton next" title="suivant">
+								<img src="assets/images/icons/next.png" alt="Next">
+							</button>
+
+							<button class="controlButton repeat" title="Activer la répétition">
+								<img src="assets/images/icons/repeat.png" alt="Repeat">
+							</button>
+
+						</div>
 
 
+						<div class="playbackBar">
 
-   </div>
-</div>
+							<span class="progressTime current">0.00</span>
 
-			<div id="nowPlayingCenter">
+							<div class="progressBar">
+								<div class="progressBarBg">
+									<div class="progress"></div>
+								</div>
+							</div>
 
-				<div class="content playerControls">
+							<span class="progressTime remaining">0.00</span>
 
-					<div class="buttons">
 
-						<button class="controlButton shuffle" title="lecture aléatoire">
-							<img src="assets/images/icons/shuffle.png" alt="Shuffle">
-						</button>
+						</div>
 
-						<button class="controlButton previous" title="Précédent">
-							<img src="assets/images/icons/previous.png" alt="Previous">
-						</button>
-
-						<button class="controlButton play" title="Lecture">
-							<img src="assets/images/icons/play.png" alt="Play">
-						</button>
-
-						<button class="controlButton pause" title="Pause " style="display: none;">
-							<img src="assets/images/icons/pause.png" alt="Pause">
-						</button>
-
-						<button class="controlButton next" title="suivant">
-							<img src="assets/images/icons/next.png" alt="Next">
-						</button>
-
-						<button class="controlButton repeat" title="Activer la répétition">
-							<img src="assets/images/icons/repeat.png" alt="Repeat">
-						</button>
 
 					</div>
 
 
-					<div class="playbackBar">
+				</div>
 
-						<span class="progressTime current">0.00</span>
+				<div id="nowPlayingRight">
+					<div class="labarredevolume">
+
+						<button class="controlButton volume" title="Volume ">
+							<img src="assets/images/icons/volume.png" alt="Volume">
+						</button>
 
 						<div class="progressBar">
 							<div class="progressBarBg">
@@ -75,40 +144,19 @@
 							</div>
 						</div>
 
-						<span class="progressTime remaining">0.00</span>
-
-
-					</div>
-
-
-				</div>
-
-
-			</div>
-
-			<div id="nowPlayingRight">
-				<div class="labarredevolume">
-
-					<button class="controlButton volume" title="Volume ">
-						<img src="assets/images/icons/volume.png" alt="Volume">
-					</button>
-
-					<div class="progressBar">
-						<div class="progressBarBg">
-							<div class="progress"></div>
-						</div>
 					</div>
 
 				</div>
 
+
+
+
 			</div>
-
-
-
 
 		</div>
+</div>
 
-	</div>
+
 
 </body>
 
